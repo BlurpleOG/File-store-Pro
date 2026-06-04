8ab0fe33b6a29bda03ac5bec472fa03ccb4ab31e
BlurpleOG <nver67414@gmail.com>
2026-06-04 19:04:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ DB_NAME = "BotifyX-Filestore"
 
 FSUBS = [[-1003483476894, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL =  -1001753514086  # just put channel id dont add ""
+DB_CHANNEL =  -1003891531589  # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
 #     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
